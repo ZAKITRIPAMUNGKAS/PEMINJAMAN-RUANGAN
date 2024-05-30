38d36b7094b663f65110e0a0261762cf3c66ed01
ZAKI TRI PAMUNGKAS <zakitripamungkas03@gmail.com>
2024-05-30 15:01:51 +0800

    upload

---
+++ Framework/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#tanggalPeminjamanDiv').datepicker({
+      format: 'yyyy-mm-dd', // Set the date format
+      autoclose: true, // Automatically close the calendar after selecting a date
+      todayHighlight: true, // Highlight today's date
+    });
+  });
+  
